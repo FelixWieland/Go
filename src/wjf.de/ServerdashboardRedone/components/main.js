1db2964eb55a96a49ec6ce94b52a9f9588b1545e
FelixWieland <felix.wieland@mail.de>
2018-08-13 22:48:58 +0200

    Created new Serverdashboard Project

---
+++ src/wjf.de/ServerdashboardRedone/components/main.js
@@ -0,0 +1,36 @@
+// Obtain the root
+const rootElement = document.getElementById('root')
+// Create a ES6 class component
+
+class List extends React.Component {
+  render() {
+      return (
+        <div className="container well">
+          <div className="row">
+          <h1>Test for: {this.props.name}</h1>
+            <ul>
+              <li>Test 1</li>
+              <li>Test 2</li>
+              <li>Test 2</li>
+            </ul>
+          </div>
+        </div>
+      );
+    }
+}
+
+// Create a function to wrap up your component
+function App(){
+  return(
+    <div>
+      <List name="Test"/>
+    </div>
+  )
+}
+
+
+// Use the ReactDOM.render to show your component on the browser
+ReactDOM.render(
+  <App />,
+  rootElement
+)
---
+++ src/wjf.de/ServerdashboardRedone/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+	"text/template"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+var tpl *template.Template //package.Type
+
+var fm = template.FuncMap{}
+
+func init() {
+	//Binds functions to templates
+	tpl = template.Must(template.New("").Funcs(fm).ParseGlob("templates/*.html"))
+}
+
+func main() {
+
+	router := httprouter.New()
+	router.GET("/plc/*path", serveFiles)
+	router.GET("/reloadTemplates", reloadTemplates)
+	router.GET("/", index)
+	http.ListenAndServe(":80", router)
+}
+
+func index(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	//Anonymouse struct Datatype
+	sDemo := struct {
+		name  string
+		lname string
+	}{
+		"James",
+		"Bond",
+	}
+
+	err := tpl.ExecuteTemplate(w, "index.html", sDemo)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func reloadTemplates(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	tpl = template.Must(template.New("").Funcs(fm).ParseGlob("templates/*.html"))
+	w.Write([]byte("Reloaded templates"))
+}
+
+func serveFiles(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	paramPath := ps.ByName("path")
+	if true {
+		http.ServeFile(w, req, strings.Replace(paramPath, "/", "", 1))
+	} else {
+		w.Write([]byte("Permission Error"))
+	}
+}
